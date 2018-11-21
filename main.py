da269417a5511f3ae8ee44d8e6ef56ab456d4e47
Abe Malla <abe.malla8@gmail.com>
2018-11-21 01:08:51 -0800

    Randomized step counts

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import config
 import random
 
-from time import sleep
+from time import sleep, time
+from datetime import datetime
 from instagram import Instagram
 
 
@@ -10,22 +11,33 @@ COMMENT_STEP = 15
 FOLLOW_STEP = 20
 
 
+def getTimeStamp():
+    ts = time()
+    return datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+
+
 def randSleep():
-    sleep(random.randint(2, 5))
+    sleep(random.randint(4, 10))
 
 
 def likePost(ig: Instagram, data: dict):
     ig.likePost(data['id'])
     mediaType = 'video' if data['is_video'] else 'image'
     shortcode = data['shortcode']
-    print(f'∘ Liked {mediaType} | https://www.instagram.com/p/{shortcode}')
+    print(
+        f'∘ [{getTimeStamp()}] Liked {mediaType} | https://www.instagram.com/p/{shortcode}')
+
+    LIKE_STEP = random.randint(2, 4)
 
 
 def commentPost(ig: Instagram, data: dict):
     c = random.choice(config.COMMENTS)
     ig.postComment(data['id'], c)
     shortcode = data['shortcode']
-    print(f'∘ Posted comment | https://www.instagram.com/p/{shortcode}')
+    print(
+        f'∘ [{getTimeStamp()}] Posted comment | https://www.instagram.com/p/{shortcode}')
+
+    COMMENT_STEP = random.randint(13, 15)
 
 
 def followPoster(ig: Instagram, shortcode: str):
@@ -34,7 +46,19 @@ def followPoster(ig: Instagram, shortcode: str):
 
     user = x['username']
     ig.followUser(user)
-    print(f'∘ Followed {user} | https://www.instagram.com/{user}')
+    print(f'∘ [{getTimeStamp()}] Followed {user} | https://www.instagram.com/{user}')
+
+    FOLLOW_STEP = random.randint(14, 20)
+
+
+def unfollowPoster(ig: Instagram, shortcode: str):
+    x = ig.getPostInfo(shortcode)
+    sleep(5)
+
+    user = x['username']
+    ig.unfollowUser(user)
+    print(
+        f'∘ [{getTimeStamp()}] Unfollowed {user} | https://www.instagram.com/{user}')
 
 
 def main():
@@ -46,31 +70,34 @@ def main():
 
     # --------------------------------------
 
-    tagFeed = []
-    for i in config.TAGS:
-        print(f'∘ Getting posts for #{i}')
-        tagFeed.extend(ig.searchTagFeed(i, count=50))
-        randSleep()
-
-    for i, x in enumerate(tagFeed):
+    print(f'\n∘ [{getTimeStamp()}] Getting timeline posts', end='\n\n')
+    timeline = ig.getTimeline(count=200)
+    for i, x in enumerate(timeline):
         if i % LIKE_STEP == 0:
             likePost(ig, x)
-        elif i % COMMENT_STEP == 0:
+        if i % COMMENT_STEP == 0:
             commentPost(ig, x)
-        elif i % FOLLOW_STEP == 0:
-            followPoster(ig, x['shortcode'])
+        if i % unfollowPoster == 0:
+            unfollowPoster(ig, x['shortcode'])
 
         randSleep()
 
     # --------------------------------------
 
-    print(f'∘ Getting timeline posts')
-    timeline = ig.getTimeline(count=200)
-    for i, x in enumerate(timeline):
+    tagFeed = []
+    for i in config.TAGS:
+        print(f'∘ [{getTimeStamp()}] Getting posts for #{i}')
+        tagFeed.extend(ig.searchTagFeed(i.replace('#', ''), count=200))
+        randSleep()
+    print()
+
+    for i, x in enumerate(tagFeed):
         if i % LIKE_STEP == 0:
             likePost(ig, x)
-        elif i % COMMENT_STEP == 0:
+        if i % COMMENT_STEP == 0:
             commentPost(ig, x)
+        if i % FOLLOW_STEP == 0:
+            followPoster(ig, x['shortcode'])
 
         randSleep()
 
